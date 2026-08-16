6ad855b09a072d3332c07d8a0a7110ff2c669012
Nandini kumari <nandnienterprises1@gmail.com>
2026-08-16 17:06:19 +0000

    control flow

---
+++ 05_control_flow/one.js
@@ -0,0 +1,62 @@
+
+// const isuserloggedin = true
+// const temperature = 41
+// if ( temperature < 50){
+//  console.log(" temperature is less than 50");
+// } else {
+// console.log(" temperature is greater than 50")
+// }
+ // == > for equal value
+ // === > for equal type also  
+
+//  const score = 200
+//  if (score > 100){ 
+//     const power = "fly"
+//     console.log(`use power:${ power}`)
+//  }
+//  console.log(`use power:${ power}`)
+
+//const balance = 1000
+//if (balance > 500) console.log("test"),console.log("test2");  
+// if ( balance < 500){
+//     console.log(" less than 500")
+// }  else if ( balance < 750){
+//     console.log(" less than 750")
+// } else if ( balance < 900){
+//     console.log(" less than 900")
+// } else{
+//     console.log(" less than 1200")
+// }
+
+
+
+// const userloggedin = true
+// const debitcard = true
+// const loggedinfromgoggle = false
+// const loggedinfromemail = true
+
+// if(userloggedin && debitcard ){
+//     console.log(" allow to buy courses");
+// }
+// if(loggedinfromgoggle || loggedinfromemail ){
+//     console.log(" user loggged in");
+// } 
+
+const month = 3
+switch (month){
+        case 1:
+            console.log("january");
+            break;
+        case 2:
+            console.log("february");
+            break;
+        case 3:
+            console.log("march");
+           break;
+        case 4:
+            console.log("april");
+            break;
+            default:
+                console.log("default case match");
+                break;
+}
